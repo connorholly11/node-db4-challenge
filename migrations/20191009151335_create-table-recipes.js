eb646c97f67ce2de61256773f5835c505fa32629
Connor Holly <connorholly11@gmail.com>
2019-10-09 16:57:50 -0400

    fixed structure

--- migrations/20191009151335_create-table-recipes.js
+++ migrations/20191009151335_create-table-recipes.js
@@ -13,10 +13,26 @@ exports.up = function(knex) {
         .string("ingredient", 128)
         .notNullable()
         .unique();
+      tbl.integer("quantity").notNullable();
+    })
+    .createTable("steps", tbl => {
+      tbl.integer("step_number").notNullable();
+      tbl.text("instruction").notNullable();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipe");
     })
     .createTable("final", tbl => {
       tbl.increments();
-      tbl.integer("quantity").notNullable();
+      tbl
+        .integer("quantity_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredients");
       tbl
         .integer("ingredient_id")
         .unsigned()
@@ -35,6 +51,7 @@ exports.up = function(knex) {
 exports.down = function(knex) {
   return knex.scheme
     .dropTableIfExists("final")
+    .dropTableIfExists("steps")
     .dropTableIfExists("ingredients")
     .dropTableIfExists("recipe");
 };
--- seeds/01-final.js
+++ seeds/01-final.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex("final").insert([
-        { quantity: 3, ingredient_id: 2, recipe_id: 3 },
-        { quantity: 3, ingredient_id: 1, recipe_id: 1 },
-        { quantity: 4, ingredient_id: 1, recipe_id: 2 }
+        { quantity_id: 3, ingredient_id: 2, recipe_id: 3 },
+        { quantity_id: 3, ingredient_id: 1, recipe_id: 1 },
+        { quantity_id: 4, ingredient_id: 1, recipe_id: 2 }
       ]);
     });
 };
