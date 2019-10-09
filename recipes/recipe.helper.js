82f28e37834aa69f9de75b0e0b376618d091210f
Connor Holly <connorholly11@gmail.com>
2019-10-09 17:53:31 -0400

    mvp

--- recipes/recipe.helper.js
+++ recipes/recipe.helper.js
@@ -2,6 +2,7 @@ const db = require("../db.config");
 
 module.exports = {
   getRecipes,
+  getRecipesById,
   getShoppingList,
   getInstructions
 };
@@ -10,6 +11,21 @@ function getRecipes() {
   return db("recipe");
 }
 
-function getShoppingList(recipe_id) {}
+function getRecipesById(id) {
+  return db("recipe").where("id", "=", id);
+}
 
-function getInstructions(recipe_id) {}
+function getShoppingList(recipe_id) {
+  return db("ingredients")
+    .join("recipe", "ingredients.recipe_id", "=", "recipe_id")
+    .select("recipe.recipe", "ingredients.*")
+    .where("recipe_id", "=", recipe_id);
+}
+
+function getInstructions(recipe_id) {
+  return db("steps")
+    .join("recipe", "steps.recipe_id", "=", "recipe_id")
+    .select("recipe.recipe", "steps.*")
+    .where("recipe_id", "=", recipe_id)
+    .orderBy("instruction");
+}
