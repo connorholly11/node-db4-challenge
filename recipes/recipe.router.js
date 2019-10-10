02be7ffdaadfe15f90e3880c4c446e033662fed1
Connor Holly <connorholly11@gmail.com>
2019-10-10 14:25:09 -0400

    got server working

--- recipes/recipe.router.js
+++ recipes/recipe.router.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const db = require("./recipe.helper");
 
-const router = express.Router;
+const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Im in recipes");
